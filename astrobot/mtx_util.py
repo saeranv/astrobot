6822c331a5bb55efc462db614ab3483d0b7cb3ab
Saeran Vasanthakumar <saeranv@gmail.com>
2020-12-31 18:49:01 -0500

    feat(mtx_util): Revised version of old matrix_utils2. With tests.

---
+++ astrobot/mtx_util.py
@@ -0,0 +1,183 @@
+import numpy as np
+
+DEFAULT_TOLERANCE = 1e-10
+
+
+"""
+Notes on vec and pnt distinction:
+
+vec refers to vectors, typically referring to surface edge direction vectors.
+pnt refers to points, typically referring to surface vertices.
+
+With respect to transformations, geometric transformations are 4 x 4 matrices, with the
+last coordinate set as a 0 for vectors, and 1 for points. This is to allow the
+translation of points (since the 4th column of a 4x4 matrix is its translation vector),
+but not vectors. Note that both vectors and points are affected by the rotation, scaling
+and other transformations that are not limited to the last column.
+
+Notes on numpy matrices:
+
+Numpy matrices are row major, but for legibility pnt and vec use row for x, y, z
+coordinates. This is why two transposes are needed during matrix transformations.
+
+.. code-block:: python
+
+mtx = np.array(
+    [[-6.,  6.,  6., -6.],
+     [ 0.,  0.,  0.,  0.],
+     [-3., -3.,  3.,  3.]])
+
+>> m[:, 0]
+[-6, 0, -3]
+
+>> m[:, 0:1]
+[[-6.],
+ [ 0.],
+ [-3.]])
+
+"""
+
+
+def vecmtx(pntmtx):
+    """Compute a matrix of non-unit direction vectors associated with pntmtx.
+
+    Can be considered the directed edges of from an array of vertices.
+
+    Args:
+        pntmtx: Array of surface vertices.
+
+    Returns:
+        Array of vc vectors:
+
+        .. code-block:: python
+
+            [vertice1 - vertice0,  # vec1
+             vertice2 - vertice1,  # vec2
+             ...,
+             verticen - verticen-1]  # vecn
+    """
+
+    # make loop
+    _pntmtx = np.concatenate((pntmtx, pntmtx[:, 0:1]), axis=1)
+
+    # n x 2 x 3 matrix, where n: |pntmtx|
+    vecmtx = [(_pntmtx[:, i], _pntmtx[:, i + 1])
+              for i in range(_pntmtx.shape[1] - 1)]
+    vecmtx = np.array(vecmtx)
+
+    # subtract vertex_n from vertex_n-1, and transpose
+    return (vecmtx[:, 1, :] - vecmtx[:, 0, :]).T
+
+
+def unit_vecmtx(vecmtx):
+    """Unitize vc vectors in vecmtx.
+
+    This is a somewhat contrived way to use matrix transformation to normalize an array
+    of vecs. But a good test of matrix intuition. The ith col vectors in the divmtx
+    multiplies by the jth column in the vecmtx s.t. each dir vector is divided by its
+    magnitude.
+    """
+    # create a n x n matrix to divide the vc vectors with
+    divmtx = np.diag(1.0 / np.linalg.norm(vecmtx, axis=0))
+
+    # (3 x n) * (n x n)
+    return np.matmul(vecmtx, divmtx)
+
+
+def corr(vec1, vec2):
+    """Correlation (between 0 and 1) for two vectors.
+
+    Vectors are unitized.
+    """
+    return np.dot(vec1 / np.linalg.norm(vec2), vec2 / np.linalg.norm(vec2))
+
+
+def is_parallel(vec1, vec2, tol=DEFAULT_TOLERANCE):
+    """Boolean classifying if two vectors are parrallel."""
+    return np.abs(1.0 - corr(vec1, vec2)) < tol
+
+
+def ortho_basis_mtx(vecmtx, calc_only_normal=False):
+    """Compute the orthogonal change of basis matrix for the surface defined by vecmtx.
+
+    It consists of an array of column, unit vectors of the x, y and normal vector of the
+    vecmtx. This matrix transforms point coordinates in the basis space back to the
+    standard basis space. Use the inv_ortho_basis_mtx function to get a transformation
+    matrix to transform from standard basis to the basis of the surface.
+
+    Args:
+        vecmtx: Array of vectors.
+        calc_only_normal: Optional argument to compute just the normal vector.
+            Default: False.
+
+    Returns:
+        Array of x-axis vector, y-axis vector and normal vector, or
+            just the normal vector if calc_only_normal is True. This
+            constitutes the change of basis matrix.
+    """
+
+    _is_parallel = lambda d: np.abs(1.0 - d) < DEFAULT_TOLERANCE
+
+    vec_idx = 0
+    d = corr(vecmtx[:, vec_idx], vecmtx[:, vec_idx+1])
+
+    # find non-collinear vecs
+    while _is_parallel(d) and vec_idx < vecmtx.shape[0]:
+        vec_idx += 1
+        d = corr(vecmtx[:, vec_idx], vecmtx[:, vec_idx+1])
+
+    # Note that the ccw order of the polygon edges results in a
+    # +z-axis normal for a xy-plane, and a -y-axis normal for a
+    # xz-plane. This corresponds with this module's assumptions
+    # of outward facing normals.
+    nvec = np.cross(vecmtx[:, vec_idx], vecmtx[:, vec_idx+1])
+
+    if calc_only_normal:
+        return nvec
+
+    # Otherwise return the yvec orthogonal to the xvec by first checking
+    # if the correlation is 0 (perpendicular), else computing the cross
+    # between the xvec and normal.
+    xvec = vecmtx[:, vec_idx]
+    if np.abs(d) < DEFAULT_TOLERANCE:
+        yvec = vecmtx[:, vec_idx+1]
+    else:
+        yvec = np.cross(nvec, xvec)
+
+    # transpose to ensure basis vectors are swapped to columns
+    return unit_vecmtx(np.array([xvec, yvec, nvec]).T)
+
+
+def inv_ortho_basis_mtx(ortho_basis_mtx):
+    """Inverse change of basis matrix.
+
+    This matrix transforms point coordinates in the standard basis to the
+    basis space defined in the ortho_basis_mtx.
+
+    Args:
+        ortho_basis_mtx: orthogonal basis matrix, calculated from ortho_basis_mtx
+            function.
+
+    Returns:
+        Inverse change of basis matrix.
+    """
+
+    return np.linalg.pinv(ortho_basis_mtx)
+
+
+def normal_vec(vecmtx):
+    """Compute the (non-unit) normal vector associated with a (non-unit) vecmtx.
+
+    Normal is computed by traversing vecs until it finds non-collinear vecs.
+    The normal vector is perpendicular to the plane between the two non-collinear
+    vectors.
+
+    Args:
+        vecmtx: Array of vectors.
+
+    Returns:
+        Normal vector associated with vecmtx.
+    """
+    return ortho_basis_mtx(vecmtx, calc_only_normal=True)
+
+
---
+++ tests/mtx_util_test.py
@@ -0,0 +1,74 @@
+import pytest
+import numpy as np
+from astrobot import mtx_util
+
+
+PNTMTX_XY = np.array(
+    [[-6.,  6.,  6., -6.],
+     [-3., -3.,  3.,  3.],
+     [ 0.,  0.,  0.,  0.]])
+
+
+PNTMTX_XZ = np.array(
+    [[-6.,  6.,  6., -6.],
+     [ 0.,  0.,  0.,  0.],
+     [-3., -3.,  3.,  3.]])
+
+
+def test_unit_vecmtx():
+    """Test unitization of matrix of direction vectors."""
+
+    unit_vecmtx = mtx_util.unit_vecmtx(mtx_util.vecmtx(PNTMTX_XY))
+    chkmtx = np.array(
+        [[ 1,  0,  0],
+         [ 0,  1,  0],
+         [-1,  0,  0],
+         [ 0, -1,  0]]).T
+
+    assert np.allclose(unit_vecmtx, chkmtx, atol=1e-10)
+
+
+def test_normal_vec():
+    """Test normal vector associated with a matrix of edge direction vectors."""
+
+    # test xy plane
+    nvec = mtx_util.normal_vec(mtx_util.vecmtx(PNTMTX_XY))
+    nvec = nvec / np.linalg.norm(nvec)
+    chkvec = np.array([0, 0, 1])
+
+    print(nvec)
+
+    assert np.allclose(nvec, chkvec, atol=1e-10)
+
+    # test xz plane
+    nvec = mtx_util.normal_vec(mtx_util.vecmtx(PNTMTX_XZ))
+    nvec = nvec / np.linalg.norm(nvec)
+    chkvec = np.array([0, -1, 0])
+
+    assert np.allclose(nvec, chkvec, atol=1e-10)
+
+
+def test_ortho_basis_mtx():
+    """Test ortho basis vectors."""
+
+    basis_mtx = mtx_util.ortho_basis_mtx(mtx_util.vecmtx(PNTMTX_XZ))
+    chk_mtx = np.array(
+        [[1,  0,  0],
+         [0,  0, -1],
+         [0,  1,  0]])
+
+    assert basis_mtx.shape[0] == 3
+    assert basis_mtx.shape[1] == 3
+    assert np.allclose(basis_mtx, chk_mtx, atol=1e-10)
+
+
+def test_inv_ortho_basis_mtx():
+    """Test inv ortho basis vectors."""
+
+    basis_mtx = mtx_util.ortho_basis_mtx(mtx_util.vecmtx(PNTMTX_XZ))
+    inv_basis_mtx = mtx_util.inv_ortho_basis_mtx(basis_mtx)
+    chk_mtx = np.eye(3)  # identity matrix
+
+    idmtx = np.matmul(basis_mtx, inv_basis_mtx)
+
+    assert np.allclose(idmtx, chk_mtx, atol=1e-10)
