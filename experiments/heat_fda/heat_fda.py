f32cb4754b0145a1d560fabb4a6141ab993f9ebc
saeranv <saeranv@gmail.com>
2021-06-07 17:09:18 -0400

    feat(experiments): platelet count model.

--- experiments/heat_fda/heat_fda.py
+++ experiments/heat_fda/heat_fda.py
@@ -66,7 +66,7 @@ def oned_loop(dimt=4, dimx=1.5, C=420, p=8, k=13.44):
     for t in range(dimt-1):
         for i in range(1, dimx-1):
             # Central difference in 1D:
-            #   T2_t1 = (-k dt / p C) (T3 - 2T2 + T1)/dx^2 + T2_t0
+            # T2_t1 = (-k dt / p C) (T3 - 2T2 + T1)/dx^2 + T2_t0
             ux[t + 1, i] = \
                 (alpha * dt) * ((ux[t, i - 1] - (2 *  ux[t, i]) + ux[t, i + 1]) / DX2) + ux[t, i]
 
@@ -103,12 +103,14 @@ def oned_mtx(dimt=4, dimx=1.5, C=420, p=8, k=13.44):
     A += np.diag(v=np.ones((dimx-1)) * dt / DX2, k=1)
 
     for t in range(dimt-1):
-        uxy[t + 1, :, :] = (A @ uxy[t, :, :]) + uxy[t, :, :]  # (dimx x dimx) @ (dimx, 1) = dimx x 1
+        # T2_t1 = (-k dt / p C) (T3 - 2T2 + T1)/dx^2 + T2_t0
+        # (dimx x dimx) @ (dimx, 1) = dimx x 1
+        uxy[t + 1, :, :] = (A @ uxy[t, :, :]) + uxy[t, :, :]
 
     return uxy
 
 
-def twod_loop(dimt=201, dimx=4.1, dimy=4.1, C=420, p=8, k=13.44):
+def twod_loop(dimt=201, dimx=4.1, dimr=1, C=420, p=8, k=13.44):
     """
     2D Heat equation with for loop.
 
@@ -126,12 +128,12 @@ def twod_loop(dimt=201, dimx=4.1, dimy=4.1, C=420, p=8, k=13.44):
     # Set initial parameters
     t_lo, t_hi = 300, 700
     alpha = _diffusivity(C, p, k)  # 4 mm/
-    dimx, dimy = int(dimx/DX), int(dimy/DX)
+    dimx = dimy = int(dimx/DX)
 
     # Set up material matrix
     uxy = np.ones((dimt, dimx, dimy)) * t_lo
     dt = _max_time_step(alpha, DX2)
-    r, cx, cy = 1, 2, 2
+    r, cx, cy = dimr, int((dimx * DX)//2), int((dimy * DX)//2)
     xx, yy = np.meshgrid(np.arange(dimx), np.arange(dimy))
     rad_mtx = (cx - xx * DX) ** 2 + (cy - yy * DX) ** 2
     uxy[0, rad_mtx < r * r] = t_hi
@@ -140,11 +142,54 @@ def twod_loop(dimt=201, dimx=4.1, dimy=4.1, C=420, p=8, k=13.44):
         for ix in range(1, dimx-1):
             for iy in range(1, dimy-1):
                 # Central difference in 2d:
-                #   T2_t1 = (Tx3 + Ty3 - 4T2 + Tx1 + Ty1) / dx^2 + T2_t0
+                #   T2_t1 = (-k dt / p C) * (Tx3 + Ty3 - 4T2 + Tx1 + Ty1) / dx^2 + T2_t0
                 ux = uxy[t, ix + 1, iy] - (2 *  uxy[t, ix, iy]) + uxy[t, ix - 1, iy]
                 uy = uxy[t, ix, iy + 1] - (2 *  uxy[t, ix, iy]) + uxy[t, ix, iy - 1]
                 uxy[t + 1, ix, iy] = \
                     (alpha * dt) * ((ux + uy) / DX2) + uxy[t, ix, iy]
+
+    return uxy
+
+
+def twod_mtx(dimt=201, dimx=4.1, dimr=1, C=420, p=8, k=13.44):
+    """
+    2D Heat equation with matrix.
+
+    Args:
+        dimx = Number of nodes in x dimension spaced by 1mm.
+        dimy = Number of nodes in y dimension spaced by 1mm.
+        C = Specific heat capacity (J/kg-K)
+        p = kg/m3
+        k = W/m2-K
+
+    Returns:
+        3D matrix storing time, x y dim.
+    """
+
+    # Set initial parameters
+    t_lo, t_hi = 300, 700
+    alpha = _diffusivity(C, p, k)  # 4 mm/
+    dimx = dimy = int(dimx/DX)
+
+    # Set up material matrix
+    uxy = np.ones((dimt, dimx, dimy)) * t_lo
+    dt = _max_time_step(alpha, DX2)
+    r, cx, cy = dimr, int((dimx * DX)//2), int((dimy * DX)//2)
+    xx, yy = np.meshgrid(np.arange(dimx), np.arange(dimy))
+    rad_mtx = (cx - xx * DX) ** 2 + (cy - yy * DX) ** 2
+    uxy[0, rad_mtx < r * r] = t_hi
+
+    for t in range(dimt-1):
+        # Central difference in 2d:
+        #   T2_t1 = (-k dt / p C) * (Tx3 + Ty3 - 4T2 + Tx1 + Ty1) / dx^2 + T2_t0
+        # We shift a dimx-1 x dimx-1 square so that the following relation occurs:
+        #   Tx3 - 2Tx2 + Tx1
+        ux = uxy[t, 2:, 1:-1] - (2 * uxy[t, 1:-1, 1:-1]) + uxy[t, :-2, 1:-1]
+        # We shift a dimx-1 x dimx-1 square so that the following reln occurs:
+        #   Ty3 - 2Ty2 + Ty1
+        uy = uxy[t, 1:-1, 2:] - (2 * uxy[t, 1:-1, 1:-1]) + uxy[t, 1:-1, :-2]
+        # Add everything up
+        uxy[t + 1, 1:-1, 1:-1] = (alpha * dt) * ((ux + uy) / DX2) + uxy[t, 1:-1, 1:-1]
 
     return uxy
 
@@ -160,31 +205,31 @@ def oned_steady_state(dimx=1.5, T0=100, TL=0, k=13.44):
         TL = Temeprature at Lth boundary condition
 
     Returns:
-        2D matrix storing x dim.
+        1D matrix storing temperatures.
     """
 
     # Set initial parameters
     print('# of nodes: {}/{} = {}'.format(dimx, DX, int(np.round(dimx/DX, 0))))
     dimx = int(np.round(dimx/DX, 0))
-    # Set up material matrix
-    # ux = np.ones((dimx, 1)) * t_lo
-    # ux[:, 0, 1: -1] = t_hi
 
-    # From pg. 325 of Cengel
+    # A: matrix of constants
+    # Based on equations on pg. 325 of Cengel
     # k(Tx3 - 2*T2 + Tx1) / dx2 + k(Ty3 - 2*T2 + Ty1) / dy2 = 0
     # k(Tx3 + Ty3 - 4*T2 + Tx1 + Ty1) / dx2 = 0
 
-    # A: matrix of constants
-    # main diagonal: -2k / dx2  (for T2)
-    # lower diagonal: k / dx2  (for T3)
-    # upper diagonal: k / dx2 (for T1)
+    # Note: can remove k/DX2 since everything is constant. It cancels out.
+    # but nice to have now in case k/DX changes.
+
     A = np.zeros((dimx, dimx))
-    A = np.diag(v=np.ones((dimx)) * -2 * k / DX2, k=0)
+    # main diagonal: -2k / dx2  (for T2s)
+    A += np.diag(v=np.ones((dimx)) * -2 * k / DX2, k=0)
+    # lower diagonal: k / dx2  (for T3s)
     A += np.diag(v=np.ones((dimx-1)) * k / DX2, k=-1)
+    # upper diagonal: k / dx2 (for T1s)
     A += np.diag(v=np.ones((dimx-1)) * k / DX2, k=1)
 
     # Add boundary
-    y = np.ones((dimx, 1))
+    y = np.zeros((dimx, 1))
     y[ 0, 0] = -k * T0 / DX2
     y[-1, 0] = -k * TL / DX2
     Ainv = np.linalg.pinv(A)
@@ -192,11 +237,126 @@ def oned_steady_state(dimx=1.5, T0=100, TL=0, k=13.44):
 
     return ux
 
+
+def _2d_adj_mtx(dimx, dimy):
+    """Construct adjacency matrix capturing 2D nodes.
+
+    For the following lattice of nodes with dimx=4, dimy=2:
+
+        N1 N2 N3 N4
+        N5 N6 N7 N8
+
+    The following matrix will be constructed:
+
+            N1  N2  N3  N4  N5  N6  N7  N8
+        N1  -4   1   0   0   1   0   0   0  = -Tup - Tleft
+        N2   1  -4   1   0   0   1   0   0  = -Tup
+        N3   0   1  -4   1   0   0   1   0  = -Tup
+        N4   0   0   1  -4   0   0   0   1  = -Tup - Tright
+        -----------------------------------
+        N5   1   0   0   0  -4   1   0   0  = -Tdown - Tleft
+        N6   0   1   0   0   1  -4   1   0  = -Tdown
+        N7   0   0   1   0   0   1  -4   1  = -Tdown
+        N8   0   0   0   1   0   0   1  -4  = -Tdown - Tright
+
+    Note that between N4 and N5 the boundary conditions are modified
+    to reflect there is no spatial adjacency between them.
+    """
+    dimn = dimx * dimy
+    A = np.zeros((dimn, dimn)).astype(np.float64)
+
+    # main diagonal for current node
+    A += np.diag(v=np.ones(dimn) * -4, k=0)
+
+    # Set left/right diagonals
+    # lower diagonal 1 for left node
+    A += np.diag(v=np.ones(dimn-1), k=-1)
+    # upper diagonal 1 for right node
+    A += np.diag(v=np.ones(dimn-1), k=1)
+
+    # Set up/down diagonals offset by number of nodes in dimx
+    # lower diagonal 2 for up node (offset by -dimx)
+    A += np.diag(v=np.ones(dimn-dimx), k=-dimx)
+    # upper diagonal 2 for down node (offset by dimx)
+    A += np.diag(v=np.ones(dimn-dimx), k=dimx)
+
+    # Remove the left/right boundary nodes at the row start/end
+    i = np.arange(1, dimy).astype(int)
+    ri = i * dimx
+    A[ri - 1, ri] = 0 # remove right-most value for top row
+    A[ri, ri - 1] = 0 # remove left-most value for bottom row
+
+    return A
+
+
+def twod_steady_state(dimx, dimy, bcs, k=13.44, qgens=None):
+    """
+    2D steady heat conduction.
+    #   T2_t1 = (Tx3 + Ty3 - 4T2 + Tx1 + Ty1) / dx^2 + T2_t0
+
+    Args:
+        k: W/m2-K
+        bcs: top, bottom, left, right boundary conditions as
+            specific temperatures.
+    Returns:
+        2D matrix storing temperatures.
+    """
+
+    # Unpack bc temperatures
+    Tup, Tdown, Tleft, Tright = bcs
+    qgens = [] if not qgens else qgens
+
+    # A: matrix of constants, but built out for 2 dimensions
+    # Based on equations on pg. 326 of Cengel
+    # k(Tx3 - 2*T2 + Tx1) / dx2 + k(Ty3 - 2*T2 + Ty1) / dy2 = 0
+    # k(Tx3 + Ty3 - 4*T2 + Tx1 + Ty1) / dx2 = 0
+
+    A = _2d_adj_mtx(dimx, dimy)
+
+    # Add constant temps from boundary
+    y = np.zeros((dimx * dimy, 1))
+
+    # Add left/right bc to left/right-most nodes
+    # get row idxs for where rows end, add -Tleft -Tright
+    row_bc_idxs = np.arange(0, dimx * dimy, dimx).astype(int)
+    # Add right most specific temp bc
+    y[row_bc_idxs - 1] += -Tright
+    # Add left most specific temp bc
+    y[row_bc_idxs] += -Tleft
+
+    # Add up/down bc to upper/lower-most nodes
+    row_bc_idxs = np.arange(dimx).astype(int)  # first dimx nodes
+    y[row_bc_idxs] += -Tup
+    row_bc_idxs = (dimx * dimy) - 1 - row_bc_idxs  # last dimx nodes
+    y[row_bc_idxs] += -Tdown
+
+    # Add a heat source
+    for ci, ri, qgen in qgens:
+       A_idx = (ri * dimx) + ci
+       y[A_idx] += -(qgen * DX2 / k)  # add heat source and convert to C
+
+    # multiply by k/DX2 (optional, comment when debugging)
+    A *= k / DX2
+    y *= k / DX2
+
+    # Solve
+    Ainv = np.linalg.pinv(A)
+    uxy = Ainv @ y
+
+    # For debugging
+    #xx = np.hstack([A, y])
+    #print(xx.astype(int))
+
+    return uxy.reshape(dimy, dimx)
+
 if __name__ == "__main__":
 
     oned_loop()
     oned_mtx()
     twod_loop()
+    twod_mtx()
+    oned_steady_state()
+
 
 
 
