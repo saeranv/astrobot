4f3b237669a94e0ce9227b1a23a05c9ce74595b4
Saeran Vasanthakumar <saeranv@gmail.com>
2022-01-09 01:48:40 -0500

    feat(pmf.py): Make joint pmf function.

--- astrobot/pmf.py
+++ astrobot/pmf.py
@@ -1,23 +1,41 @@
 """Distributions from empirical datasets"""
 import numpy as np
 import pandas as pd
-from typing import Set, Sequence
+from typing import Sequence, Callable
+from pprint import pprint
+from functools import reduce
+
+
+def pp(x, *args):
+    pprint(x) if not args else (pprint(x), pp(*args))
 
 
 class Pmf1(object):
     """One-dimensional Probability Mass Function (pmf) for discrete data."""
-    # TODO: add bld_idx
-    def __init__(self, qs:np.ndarray, ps:np.ndarray, id:np.ndarray, bins:int) -> None:
+
+    def __init__(self, qs:np.ndarray, bins:int, ps:np.ndarray=None,
+                 bin_stat_fx:Callable=None, id:np.ndarray=None) -> None:
         """Initialize pmf properties."""
-        self.qs_arr = qs if isinstance(qs, np.ndarray) else np.array(qs)
-        self.ps_arr = ps if isinstance(ps, np.ndarray) else np.array(ps)
-        self.id_arr= np.array(id, dtype=int)
-        self.N = np.sum(self.ps_arr)  # total var dataset size
+
+        # Required args
+        self.qs_arr = _seq2arr(qs)
         self.bins = bins
+        self._bin_stat = np.mean if bin_stat_fx is None else bin_stat_fx
+
+        # Optional args
+        data_num = self.qs_arr.shape[0]
+        self.ps_arr = np.ones(data_num) / data_num \
+                      if ps is None else _seq2arr(ps)
+        self._id_arr = np.arange(data_num, dtype=int) if id is None \
+                       else np.array(id, dtype=int)
+        self.N = np.sum(self.ps_arr)  # total var dataset size
+
+        # Cached args
         self._bin_edges = None
-        self._bin_mu = None
         self._bin_idx = None
         self._df = None
+
+        # Formatting
         self.prec = 2
 
     def __repr__(self) -> str:
@@ -32,24 +50,8 @@ class Pmf1(object):
         dist.bin_idx: [[0], [1, 3], [2, 4]]
         """
         if self._bin_idx is None:
-            bin_data = make_bin_data(
-                self.qs_arr, bins=self.bins, bin_range=(self.qs_arr.min(), self.qs_arr.max()))
-            self._bin_idx, self._bin_mu, self._bin_edges = bin_data
+            self._bin_idx = make_bin_idx(self.qs_arr, self.bin_edges)
         return self._bin_idx
-
-    @property
-    def bin_mu(self):
-        """Array of mean qs of bin.
-
-        qs = [0, 1, 3, 1, 2]
-        dist = Pmf1(qs, ps, bins=3)
-        dist.bin_mu: [0.5, 1.5, 2.5]
-        """
-        if self._bin_mu is None:
-            bin_data = make_bin_data(
-                self.qs_arr, bins=self.bins, bin_range=(self.qs_arr.min(), self.qs_arr.max()))
-            self._bin_idx, self._bin_mu, self._bin_edges = bin_data
-        return self._bin_mu
 
     @property
     def bin_edges(self):
@@ -60,58 +62,78 @@ class Pmf1(object):
         dist.bin_edges: [0, 1, 2, 3]
         """
         if self._bin_edges is None:
-            bin_data = make_bin_data(
-                self.qs_arr, bins=self.bins, bin_range=(self.qs_arr.min(), self.qs_arr.max()))
-            self._bin_idx, self._bin_mu, self._bin_edges = bin_data
+            self._bin_edges = make_bin_edges(
+                self.qs_arr, bins=self.bins,
+                bin_range=(self.qs_arr.min(), self.qs_arr.max()))
         return self._bin_edges
 
     @property
     def df(self):
         if self._df is None:
-            self._df = _make_pmf1(self.qs_arr, self.ps_arr, self.bin_idx, self.bin_mu)
+            self._df = _make_pmf1(self.qs_arr, self.ps_arr, self.bin_idx,
+                self.bin_edges, self._bin_stat)
         return self._df
 
-def _make_pmf1(qs:np.ndarray, ps:np.ndarray, bin_idx:Sequence, bin_mu:np.ndarray) -> np.ndarray:
+
+def _seq2arr(seq:Sequence) -> np.ndarray:
+    """Cast to np.ndarray else throw exception."""
+    seq = seq if isinstance(seq, np.ndarray) else np.array(seq)
+    assert isinstance(seq, np.ndarray), "Input sequence must be " \
+        "np.ndarray, got {}.".format(seq)
+    return seq
+
+
+def _make_pmf1(qs:np.ndarray, ps:np.ndarray, bin_idx:Sequence,
+               bin_edges:np.ndarray, bin_stat_fx:Callable) -> np.ndarray:
     """Pmf1 object from empirical data."""
 
     N = np.sum(ps)  # total var dataset size
     bins = len(bin_idx)
-    binps_arr = [np.sum(ps[bi]) for bi in bin_idx]
+
+    bin_ps = [np.sum(ps[bi]) for bi in bin_idx]
+    bin_mu = [bin_stat_fx(bin_edges[[i-1, i]])
+              for i in range(1, bins + 1)]
 
     # Create initial dataframe
     bin_df = pd.DataFrame(
         {"bins": range(bins),
-         "ps": np.array(binps_arr) / N, # normalize by dataset size
-         "qs": bin_mu})
-
+         "ps": np.array(bin_ps) / N, # normalize by dataset size
+         "qs": np.array(bin_mu)})
+    bin_df.set_index("bins", inplace=True)
     return bin_df
 
-def make_bin_data(vals:np.ndarray, bins:int, bin_range:Sequence[float], eps=1e-10) -> Sequence[float]:
-    """Creates bins from vals."""
+
+def make_bin_edges(vals:np.ndarray, bins:int, bin_range:Sequence[float]) -> np.ndarray:
+    """Create bin_edges."""
     # Get bin_edges
     min_edge, max_edge = bin_range
     bin_inc = (max_edge - min_edge) / bins
-    bin_edges = min_edge + np.array([bin_inc * i for i in range(bins + 1)])
+    return min_edge + np.array([bin_inc * i for i in range(bins + 1)])
 
-    # Get bin_mu
-    bin_mu = np.array([np.mean(bin_edges[[i-1, i]])
-                       for i in range(1, bins + 1)])
 
-    # Get binnumber
+def make_bin_idx(qs:np.ndarray, bin_edges:np.ndarray, eps=1e-10) -> Sequence:
+    """Creates bin_idx, a list of arrays from qs. Arrays are of unequal length."""
+
+    bins = len(bin_edges) - 1
     bin_edges[-1] += eps  # to force last datapoint in
-    _in_bin_fx = lambda v, lo, hi: np.where((v >= lo) & (v < hi))[0]
-    bin_idx = [_in_bin_fx(vals, bin_edges[i-1], bin_edges[i]).tolist()
+
+    # Filter by bin_edges
+    _in_bin_fx = lambda v, lo, hi: np.where((v >= lo) & (v < hi))
+    bin_idx = [_in_bin_fx(qs, bin_edges[i-1], bin_edges[i])
                for i in range(1, bins + 1)]
 
-    return bin_idx, bin_mu, bin_edges
+    # Clean datatype
+    return [bi[0].astype(int).tolist() for bi in bin_idx]
 
-def prec(pmf_df, precision:int=2) -> pd.DataFrame:
+
+def prec(pmf_df: pd.DataFrame, precision:int=2) -> pd.DataFrame:
     """Mutates precisions in Pmf for pretty printing."""
     pmf_df_ = pmf_df.copy()
     pmf_df_.loc[:, "ps"] = np.round(pmf_df_["ps"].values, precision)
     return pmf_df_
 
 
+# TODO move this to viz
 import matplotlib.pyplot as plt
 
 def plt_hist(pmf, **kwargs):
--- tests/pmf_test.py
+++ tests/pmf_test.py
@@ -17,8 +17,10 @@ def test_make_pmf1():
     data = _make_dataset()
     qs, ps = data.qs.values, data.ps.values
 
-    bin_idx, bin_mu, _ = pmf.make_bin_data(qs, bins=3, bin_range=(qs.min(), qs.max()))
-    df = pmf._make_pmf1(qs, ps, ps, bin_idx, bin_mu)
+    bin_edges = pmf.make_bin_edges(qs, bins=3, bin_range=(qs.min(), qs.max()))
+    bin_idx = pmf.make_bin_idx(qs, bin_edges)
+
+    df = pmf._make_pmf1(qs, ps, bin_idx, bin_edges)
 
     # Correct values
     bin_idx = [0, 1, 2]
@@ -29,9 +31,10 @@ def test_make_pmf1():
     assert np.allclose(ps, ps, atol=1e-10)
 
 
-def test_make_bin_data():
-    """Test binnumber and bin_edges"""
+def test_make_bin_edges():
+    """Test bin_edges"""
 
+    # Dataset to bin
     #     0  1  2  3  4  5  6  7  8
     qs = [1, 1, 2, 3, 2, 5, 3, 1, 6]
     # 1: [1, 1, 1]
@@ -41,8 +44,7 @@ def test_make_bin_data():
     # 5: [5]
     #    [1, 1, 2, 3, 2, 5, 3, 1, 5]
     bins=5; qs = np.array(qs)
-    bin_data = pmf.make_bin_data(qs, bins=bins, bin_range=(qs.min(), qs.max()))
-    tst_bin_idx, tst_bin_mu, tst_bin_edges = bin_data
+    tst_bin_edges = pmf.make_bin_edges(qs, bins=bins, bin_range=(qs.min(), qs.max()))
 
     # Correct data
     binstat = binned_statistic(qs, qs, bins=bins)
@@ -50,28 +52,16 @@ def test_make_bin_data():
     # Test bin_edge
     assert np.allclose(tst_bin_edges, binstat.bin_edges, atol=1e-10)
 
-    # Test bin_mu
-    bin_mu = [1.5, 2.5, 3.5, 4.5, 5.5]
-    assert np.allclose(tst_bin_mu, bin_mu, atol=1e-10), tst_bin_mu
-
-    # Tests binnumber
-    tst_bin_arr = [qs[bi] for bi in tst_bin_idx]
-    binnumber_ = binstat.binnumber - 1
-    bin_arr = [qs[np.where(binnumber_ == bi)[0]]
-               for bi in range(binnumber_.max())]
-    tst_bin_idx_ = [np.allclose(tst_bin, bin, atol=1e-10)
-                    for tst_bin, bin in zip(tst_bin_arr, bin_arr)]
-    assert np.all(tst_bin_idx_)
-
 
 def test_make_bin_idx():
     """Test _make_idx."""
 
+    # Test 1
     data = _make_dataset()
     qs, ps = data.qs.values, data.ps.values
     binstat = binned_statistic(qs, qs, bins=3, statistic="mean")
 
-    bin_idx, _, _ = pmf.make_bin_data(qs, bins=3, bin_range=(qs.min(), qs.max()))
+    bin_idx = pmf.make_bin_idx(qs, bin_edges=binstat.bin_edges)
     tst_binned_qs = [qs[bi] for bi in bin_idx]
     tst_binned_ps = [ps[bi] for bi in bin_idx]
 
@@ -85,3 +75,22 @@ def test_make_bin_idx():
             for tst_qs, qs in zip(tst_binned_qs, binned_qs)]
     assert [np.allclose(tst_ps, ps, atol=1e-10)
             for tst_ps, ps in zip(tst_binned_ps, binned_ps)]
+
+    # Test 2
+    #     0  1  2  3  4  5  6  7  8
+    qs = [1, 1, 2, 3, 2, 5, 3, 1, 6]
+    # [1, 1, 1], [2, 2], [3, 3], [], [5]
+    qs = np.array(qs)
+    bin_edges = np.array([1, 2, 3, 4, 5, 6])
+    binnumber = np.array([1, 1, 2, 3, 2, 5, 3, 1, 5]) - 1
+
+    # Make data
+    tst_bin_idx = pmf.make_bin_idx(qs, bin_edges)
+    tst_bin_arr = [qs[bi] for bi in tst_bin_idx]
+    bin_arr = [qs[np.where(binnumber == bi)[0]]
+               for bi in range(binnumber.max())]
+
+    # Assert
+    tst_bin_idx_ = [np.allclose(tst_bin, bin, atol=1e-10)
+                    for tst_bin, bin in zip(tst_bin_arr, bin_arr)]
+    assert np.all(tst_bin_idx_)
